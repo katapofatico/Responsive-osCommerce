b91ae54a010ade96c046584995f1e58a84fe4c6f
katapofatico <katapofatico@gmail.com>
2015-11-23 19:06:08 +0100

    Revert compatibility.php Maybe in this file th $HTTP_xxx_VARS variables are important to old osc-addons

--- includes/functions/compatibility.php
+++ includes/functions/compatibility.php
@@ -30,23 +30,23 @@
   }
 
   if (PHP_VERSION >= 4.1) {
-    $_GET =& $_GET;
-    $_POST =& $_POST;
-    $_COOKIE =& $_COOKIE;
-    $_SESSION =& $_SESSION;
-    $_FILES =& $_FILES;
+    $HTTP_GET_VARS =& $_GET;
+    $HTTP_POST_VARS =& $_POST;
+    $HTTP_COOKIE_VARS =& $_COOKIE;
+    $HTTP_SESSION_VARS =& $_SESSION;
+    $HTTP_POST_FILES =& $_FILES;
     $HTTP_SERVER_VARS =& $_SERVER;
   } else {
-    if (!is_array($_GET)) $_GET = array();
-    if (!is_array($_POST)) $_POST = array();
-    if (!is_array($_COOKIE)) $_COOKIE = array();
+    if (!is_array($HTTP_GET_VARS)) $HTTP_GET_VARS = array();
+    if (!is_array($HTTP_POST_VARS)) $HTTP_POST_VARS = array();
+    if (!is_array($HTTP_COOKIE_VARS)) $HTTP_COOKIE_VARS = array();
   }
 
 // handle magic_quotes_gpc turned off.
   if (!get_magic_quotes_gpc()) {
-    do_magic_quotes_gpc($_GET);
-    do_magic_quotes_gpc($_POST);
-    do_magic_quotes_gpc($_COOKIE);
+    do_magic_quotes_gpc($HTTP_GET_VARS);
+    do_magic_quotes_gpc($HTTP_POST_VARS);
+    do_magic_quotes_gpc($HTTP_COOKIE_VARS);
   }
 
 // set default timezone if none exists (PHP 5.3 throws an E_WARNING)
